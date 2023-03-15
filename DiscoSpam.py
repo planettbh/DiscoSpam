7dfdd0e739fa988710c1c7d219a1f2bce05d2baa
planettbh <74864510+planettbh@users.noreply.github.com>
2023-03-14 21:38:06 -0500

    1.0 Release
    Release

---
+++ DiscoSpam.py
@@ -0,0 +1,138 @@
+import pyautogui
+import string
+import time
+
+print("DiscoSpam | Welcome to DiscoSpam, User.")
+custommessage = input("[?] Custom Spam: ")
+while custommessage == "":
+    custommessage = input("[?] Custom Spam: ")
+spamdelay = float(input("[?] Delay: "))
+while spamdelay == "":
+    spamdelay = int(input("[?] Delay: "))
+
+output = custommessage + " [discospam]"
+
+print("Spam starting in 5 seconds. Please tab into discord.")
+time.sleep(5)
+
+
+while True:
+
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    pyautogui.typewrite(output)
+    time.sleep(0.01)
+    pyautogui.hotkey('enter')
+    print("[>] Sent message '" + custommessage + "'")
+    
+    time.sleep(spamdelay)
+    
+    print("[#] Sleeping for 10s to prevent discord lag/rate limits")
+    time.sleep(10)
